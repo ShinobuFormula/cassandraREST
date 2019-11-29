1ffc31cd3f111013223b13275034c993bd67d454
Damien LAPORTE <damien.laporte@viacesi.fr>
2019-11-29 11:48:35 +0100

    Adding the user_by_name and user_by_member
    +Route
    +Function

--- app/controllers/meal.go
+++ app/controllers/meal.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gocql/gocql"
 	"github.com/revel/revel"
 	"log"
@@ -85,6 +86,74 @@ func (c Meal) MealByGroup() revel.Result{
 	data := make(map[string]interface{})
 
 	if err := session.Query(`SELECT * FROM meal_by_group WHERE group = ?`, param["id"]).Consistency(gocql.One).MapScan(data); err != nil {
+		log.Fatal(err)
+	}
+
+	return c.RenderJSON(data)
+}
+
+
+func (c Meal) MealByName() revel.Result{
+	cluster := gocql.NewCluster("192.168.109.137")
+	cluster.Keyspace = "squeat_db"
+	cluster.Consistency = gocql.One
+	session, err := cluster.CreateSession()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer session.Close()
+
+	param := make(map[string]interface{})
+	c.Params.BindJSON(&param)
+
+	data := make(map[string]interface{})
+	stuff := make(map[string]interface{})
+	i := 0
+
+	iter := session.Query(`SELECT * FROM  meal_by_name WHERE id = ? AND name = ?`, param["id"], param["name"]).Consistency(gocql.One).Iter()
+	for iter.MapScan(stuff) {
+		fmt.Println(i)
+		data[strconv.Itoa(i)] = stuff
+		i++
+		stuff = make(map[string]interface{})
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return c.RenderJSON(data)
+
+}
+
+func (c Meal) MealByMember() revel.Result{
+	cluster := gocql.NewCluster("192.168.109.137")
+	cluster.Keyspace = "squeat_db"
+	cluster.Consistency = gocql.One
+	session, err := cluster.CreateSession()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer session.Close()
+
+	param := make(map[string]interface{})
+	c.Params.BindJSON(&param)
+
+	data := make(map[string]interface{})
+	stuff := make(map[string]interface{})
+	i := 0
+
+	iter := session.Query(`SELECT * FROM  meal_by_id WHERE members CONTAINS ?`, param["id"]).Consistency(gocql.One).Iter()
+	for iter.MapScan(stuff) {
+		fmt.Println(i)
+		data[strconv.Itoa(i)] = stuff
+		i++
+		stuff = make(map[string]interface{})
+	}
+	if err := iter.Close(); err != nil {
 		log.Fatal(err)
 	}
 
