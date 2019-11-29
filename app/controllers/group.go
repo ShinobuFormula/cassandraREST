2f02f8b1e0ccc8c87da69be0e84f31d31b3675d7
Damien LAPORTE <damien.laporte@viacesi.fr>
2019-11-29 12:51:54 +0100

    Adding group_by_id and group_by_leader
    +Route
    +Function

--- app/controllers/group.go
+++ app/controllers/group.go
@@ -40,3 +40,61 @@ func (c Group) AllGroup() revel.Result{
 
 	return c.RenderJSON(data)
 }
+
+func (c Group) GroupById() revel.Result{
+	cluster := gocql.NewCluster("192.168.109.137")
+	cluster.Keyspace = "squeat_db"
+	cluster.Consistency = gocql.One
+	session, err := cluster.CreateSession()
+
+	param := make(map[string]interface{})
+
+	c.Params.BindJSON(&param)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer session.Close()
+
+	data := make(map[string]interface{})
+
+	if err := session.Query(`SELECT * FROM group_by_id WHERE id = ?`, param["id"]).Consistency(gocql.One).MapScan(data); err != nil {
+		log.Fatal(err)
+	}
+
+	return c.RenderJSON(data)
+}
+
+func (c Group) GroupByLeader() revel.Result{
+	cluster := gocql.NewCluster("192.168.109.137")
+	cluster.Keyspace = "squeat_db"
+	cluster.Consistency = gocql.One
+	session, err := cluster.CreateSession()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer session.Close()
+
+	param := make(map[string]interface{})
+
+	c.Params.BindJSON(&param)
+
+	data := make(map[string]interface{})
+	stuff := make(map[string]interface{})
+	i := 0
+
+	iter := session.Query(`SELECT * FROM group_by_leader WHERE leader = ?`, param["id"]).Consistency(gocql.One).Iter()
+	for iter.MapScan(stuff){
+		data[strconv.Itoa(i)] = stuff
+		i++
+		stuff = make(map[string]interface{})
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return c.RenderJSON(data)
+}
