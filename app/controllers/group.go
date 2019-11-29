d6192f42a9f57c650827de813898eb4332fed0a0
Damien LAPORTE <damien.laporte@viacesi.fr>
2019-11-29 12:57:26 +0100

    Adding group_by_member route and function

--- app/controllers/group.go
+++ app/controllers/group.go
@@ -98,3 +98,36 @@ func (c Group) GroupByLeader() revel.Result{
 
 	return c.RenderJSON(data)
 }
+
+func (c Group) GroupByMember() revel.Result{
+	cluster := gocql.NewCluster("192.168.109.137")
+	cluster.Keyspace = "squeat_db"
+	cluster.Consistency = gocql.One
+	session, err := cluster.CreateSession()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer session.Close()
+
+	param := make(map[string]interface{})
+
+	c.Params.BindJSON(&param)
+
+	data := make(map[string]interface{})
+	stuff := make(map[string]interface{})
+	i := 0
+
+	iter := session.Query(`SELECT * FROM  group_by_id WHERE members CONTAINS ?`, param["id"]).Consistency(gocql.One).Iter()
+	for iter.MapScan(stuff) {
+		data[strconv.Itoa(i)] = stuff
+		i++
+		stuff = make(map[string]interface{})
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return c.RenderJSON(data)
+}
